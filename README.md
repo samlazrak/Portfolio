1ee021b2c82624ca9c463ca85ef2d2c551577dea
Sam Lazrak <Lazrak13@uab.edu>
2019-10-24 06:01:57 -0500

    new

---
+++ README.md
@@ -0,0 +1,1 @@
+# Samlazrak.com
---
+++ colors.js
@@ -0,0 +1,20 @@
+module.exports = {
+  background: '#322a2c',
+  /* Front main inner corners */
+  backgroundDark: '#adc586',
+
+  /* Primary text */
+  primary: '#322a2c',
+  /* Some highlights and underlines */
+  primaryLight: '#BE5157',
+  /* Top Right Corner */
+  primaryDark: '#619C99',
+
+  /* Top left corner */
+  secondary: '#83a06c',
+  /* About lower corner */
+  secondaryLight: '#72A89C',
+  /* Secondary Texts */
+  secondaryDark: '#916255',
+
+};
